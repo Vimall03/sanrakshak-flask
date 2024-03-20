66b06a3161b000a4b1bc09c22af215ecc7aa864d
Vimal M <109758195+Vimall03@users.noreply.github.com>
2024-03-20 15:43:21 +0530

    CORS setup

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return "Hello from Flask"
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Handle file upload logic here
+        file = request.files['file']
+        # Process the file as needed
+        print('file recieved')
+        
+        return jsonify('Files recieved Successfully')
     
 if __name__ == "__main__":
-    app.run(debug = True)    
+    app.run(host='localhost', port=5000, debug=True)  
